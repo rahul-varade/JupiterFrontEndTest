b56735f1d1961e393fe6689b92812b44bbd13af8
rahul-varade <rahul.varade@jupiter.money>
2022-07-14 20:36:58 +0530

    Added conditional platform test execution

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,7 +1,11 @@
 package hooks;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.sql.Timestamp;  
 import java.time.Instant;  
@@ -17,6 +21,8 @@ public class Hooks {
 	private AppiumDriver driver;
 	private final TestContext context;
 	Timestamp instant= Timestamp.from(Instant.now());  
+	FileReader reader;
+	Properties propFile;
 	public static String userName = "rahulvarade_aAw5dM";
 	public static String accessKey = "pss8VjjgENNNZc15ykar";
 	
@@ -25,30 +31,38 @@ public class Hooks {
 	}
 	
 	@Before
-	public void setUp() throws MalformedURLException {
+	public void setUp() throws IOException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		
-// Local test execution capabilities
-		capabilities.setCapability("platformName", "Android");
-		capabilities.setCapability("platformVersion", "12");
-		capabilities.setCapability("deviceName", "RZ8R120RBWA");
-		capabilities.setCapability("appPackage","money.jupiter.staging");
-		capabilities.setCapability("appActivity","money.jupiter.MainActivity");
-//		capabilities.setCapability("app", System.getProperty("user.dir")+"/app/jupiter-staging.apk");
-//		capabilities.setCapability("appWaitDuration", 120000);
-		driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"),capabilities);
-	    
-// Browser Stack capabilities	
-//		capabilities.setCapability("os_version", "11.0");
-//		capabilities.setCapability("device", "Samsung Galaxy M32");
-//		capabilities.setCapability("app", "bs://7a66b5a81f84fa3b719e5ddb0b35d7c9fde85af6");
-//		capabilities.setCapability("project", "Login");
-//		capabilities.setCapability("build", "Version-1.5.42-"+instant);
-//		capabilities.setCapability("name", "LoginTest");
-//		capabilities.setCapability("browserstack.user", "rahulvarade_aAw5dM");
-//		capabilities.setCapability("browserstack.key", "pss8VjjgENNNZc15ykar");
-//		capabilities.setCapability("browserstack.timezone", "Kolkata");
-//		driver = new AppiumDriver(new URL("http://hub.browserstack.com/wd/hub"), capabilities);
+		reader=new FileReader(System.getProperty("user.dir")+"/src/test/java/Utils/runConfig.properties");  
+		propFile=new Properties();  
+	    propFile.load(reader);  
+	 
+		if (propFile.getProperty("testExecutionPlatform").equals("local")){
+			// Local test execution capabilities
+			capabilities.setCapability("platformName", "Android");
+			capabilities.setCapability("platformVersion", "12");
+			capabilities.setCapability("deviceName", "RZ8R120RBWA");
+			capabilities.setCapability("appPackage","money.jupiter.staging");
+			capabilities.setCapability("appActivity","money.jupiter.MainActivity");
+//			capabilities.setCapability("app", System.getProperty("user.dir")+"/app/jupiter-staging.apk");
+//			capabilities.setCapability("appWaitDuration", 120000);
+			driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"),capabilities);
+		}
+
+		if (propFile.getProperty("testExecutionPlatform").equals("browserstack")) {
+			// Browser Stack capabilities	
+			capabilities.setCapability("os_version", "11.0");
+			capabilities.setCapability("device", "Samsung Galaxy M32");
+			capabilities.setCapability("app", "bs://7a66b5a81f84fa3b719e5ddb0b35d7c9fde85af6");
+			capabilities.setCapability("project", "Login");
+			capabilities.setCapability("build", "Version-1.5.42-"+instant);
+			capabilities.setCapability("name", "LoginTest");
+			capabilities.setCapability("browserstack.user", "rahulvarade_aAw5dM");
+			capabilities.setCapability("browserstack.key", "pss8VjjgENNNZc15ykar");
+			capabilities.setCapability("browserstack.timezone", "Kolkata");
+			driver = new AppiumDriver(new URL("http://hub.browserstack.com/wd/hub"), capabilities);
+		}
+
 		
 		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
 		context.driver = driver;
