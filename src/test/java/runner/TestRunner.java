62093fd0e6b6070a047ae0b79f591fdae6720330
rahul-varade <87297200+rahul-varade@users.noreply.github.com>
2022-07-19 12:26:09 +0530

    Added help center TCs  to CI

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber/cucumber.html"}, glue = {"scenarios", "hooks"}
-, features = "src/test/java/scenarios/features/", tags = "@C15111")
+, features = "src/test/java/scenarios/features/", tags = "@e2e")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
