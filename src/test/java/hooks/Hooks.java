a5ac129b318e97c33419525a3b24c23b377e43aa
rahul-varade <rahul.varade@jupiter.money>
2022-07-20 23:37:11 +0530

    Added hooks for getting scenario name in BS platform

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -15,7 +15,9 @@ import org.testng.annotations.BeforeClass;
 
 import context.TestContext;
 import io.appium.java_client.AppiumDriver;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hooks {
 	private AppiumDriver driver;
@@ -23,6 +25,7 @@ public class Hooks {
 	Timestamp instant= Timestamp.from(Instant.now());  
 	FileReader reader;
 	Properties propFile;
+	String scenario;
 	public static String userName = "rahulvarade_aAw5dM";
 	public static String accessKey = "pss8VjjgENNNZc15ykar";
 	
@@ -30,12 +33,18 @@ public class Hooks {
 		this.context = context;
 	}
 	
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+	
 	@Before
-	public void setUp() throws IOException {
+	public void setUp(Scenario scenario) throws IOException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		reader=new FileReader(System.getProperty("user.dir")+"/src/test/java/Utils/runConfig.properties");  
 		propFile=new Properties();  
 	    propFile.load(reader);  
+	    this.scenario = scenario.getName();
 	 
 		if (propFile.getProperty("testExecutionPlatform").equals("local")){
 			// Local test execution capabilities
@@ -55,8 +64,10 @@ public class Hooks {
 			capabilities.setCapability("device", "Samsung Galaxy M32");
 			capabilities.setCapability("app", "bs://7a66b5a81f84fa3b719e5ddb0b35d7c9fde85af6");
 			capabilities.setCapability("project", "Login");
-			capabilities.setCapability("build", "Version-1.5.42-"+instant);
-			capabilities.setCapability("name", "LoginTest");
+//			capabilities.setCapability("build", "Version-1.5.42-"+instant);
+			capabilities.setCapability("build", "Rewards FAQs");
+			capabilities.setCapability("name", this.scenario);
+			capabilities.setCapability("browserstack.debug","true");
 			capabilities.setCapability("browserstack.user", "rahulvarade_aAw5dM");
 			capabilities.setCapability("browserstack.key", "pss8VjjgENNNZc15ykar");
 			capabilities.setCapability("browserstack.timezone", "Kolkata");
